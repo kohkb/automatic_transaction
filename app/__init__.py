e0a212e6ba691cef4c09f3b5d4313cc506b30d35
kohkb <koh.plate@gmail.com>
2020-06-06 19:21:31 +0900

    Add Pipfile

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
@@ -7,6 +7,8 @@ from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
+# config.pyをconfigとして扱うという設定
 myapp = Flask(__name__) 
+myapp.config.from_object('app.config')
 
-from app import views
+import app.views
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from app import myapp
 
 if __name__ == "__main__":
-  myapp.run(debug=True)
+  myapp.run()
 
 
