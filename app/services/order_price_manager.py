e1e0608de79d6f0e36a3db8fb507c2cd4960dba6
kohkb <koh.plate@gmail.com>
2020-09-14 09:31:26 +0900

    Add loss price method to be extensible

--- app/services/order_price_manager.py
+++ app/services/order_price_manager.py
@@ -3,5 +3,9 @@ from app.models.prices import Price
 class OrderPriceManager():
     def __init__(self):
         self.order_price = round(Price.query.order_by(Price.id.desc()).first().ask, 3)
-        self.stop_loss_price = round(self.order_price - 0.2, 3)
-        self.take_profit_price = round(self.order_price + 0.05, 3)
+
+    def stop_loss_price(self):
+        return round(self.order_price - 0.2, 3)
+    
+    def take_profit_price(self):
+        return round(self.order_price + 0.05, 3)
